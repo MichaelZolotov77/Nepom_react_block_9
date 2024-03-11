9d0c3db3fecdf23a0812e53a334b4adad0986e9f
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-11 08:55:14 +0300

    Создание и доработка редьюсера

--- src/context.js
+++ src/context.js
@@ -1,10 +1,41 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
 
 export const ShopContext = createContext();
 
+const initialState = {
+  goods: [],
+  loading: true,
+  order: [],
+  isBasketShow: false,
+  alertName: "",
+};
+
 export const ContextProvider = ({ children }) => {
-  const value = {
-    example: "hello from context",
+  const [value, dispatch] = useReducer(reducer, initialState);
+
+  value.closeAlert = () => {
+    dispatch({ type: "CLOSE_ALERT" });
+  };
+
+  value.addToBasket = (item) => {
+    dispatch({ type: "ADD_TO_BASKET", payload: { id: item } });
+  };
+
+  value.incQuantity = (itemId) => {
+    dispatch({ type: "INCREMENT_QUANTITY", payload: { id: itemId } });
+  };
+
+  value.decQuantity = (itemId) => {
+    dispatch({ type: "DECREMENT_QUANTITY", payload: { id: itemId } });
+  };
+
+  value.removeFromBasket = (itemId) => {
+    dispatch({ type: "REMOVE_FROM_BASKET", payload: { id: itemId } });
+  };
+
+  value.handleBasketShow = () => {
+    dispatch({ type: "TOGGLE_BASKET" });
   };
 
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
